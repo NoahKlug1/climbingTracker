fbe331f4466db2978700c042cadb1fc3a0f4fe43
Noah Klug <n.klug@htl-leonding.ac.at>
2026-05-13 16:13:34 +0200

    database

--- script.js
+++ script.js
@@ -1035,65 +1035,64 @@ window.addEventListener('beforeinstallprompt', e => {
   deferredPrompt = e;
 });
 
+import { createClient }
+from 'https://esm.sh/@supabase/supabase-js'
 const supabaseUrl = "https://fdgtzilbwjiuetrheoly.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkZ3R6aWxid2ppdWV0cmhlb2x5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjczOTksImV4cCI6MjA5NDI0MzM5OX0.4SPYm4eypp7o143faPEkLAltsptS6iT1JHAitfhQEfY"
 
-const client = supabase.createClient(
+const supabase = createClient(
   supabaseUrl,
   supabaseKey
 )
 
+const status = document.getElementById("status")
+
+document
+.getElementById("registerBtn")
+.addEventListener("click", register)
+
+document
+.getElementById("loginBtn")
+.addEventListener("click", login)
+
+document
+.getElementById("logoutBtn")
+.addEventListener("click", logout)
+
 async function register(){
 
-  const email = document.getElementById("email").value
-  const password = document.getElementById("password").value
+  const email =
+    document.getElementById("email").value
 
-  const { data, error } = await client.auth.signUp({
-    email,
-    password
-  })
-
-  if(error){
-    document.getElementById("status").innerText = error.message
-    return
-  }
-
-  document.getElementById("status").innerText =
-    "Registrierung erfolgreich!"
-}
-
-async function login(){
-
-  const email = document.getElementById("email").value
-  const password = document.getElementById("password").value
+  const password =
+    document.getElementById("password").value
 
   const { data, error } =
-    await client.auth.signInWithPassword({
+    await supabase.auth.signUp({
       email,
       password
     })
 
   if(error){
-    document.getElementById("status").innerText = error.message
+    status.innerText = error.message
     return
   }
 
-  document.getElementById("status").innerText =
-    "Eingeloggt!"
+  status.innerText =
+    "Registrierung erfolgreich!"
 }
 
-async function logout(){
+async function login(){
 
-  await client.auth.signOut()
+  const email =
+    document.getElementById("email").value
 
-  document.getElementById("status").innerText =
-    "Ausgeloggt!"
-}
+  const password =
+    document.getElementById("password").value
 
-const {
-  data: { session }
-} = await client.auth.getSession()
-
-if(session){
-  console.log("User eingeloggt")
-}
+  const { data, error } =
+    await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+  }
